1593db9a4bf86313266c2475fc72e8328eefa463
Steven Black <genseb7@gmail.com>
2021-03-09 18:38:01 -0500

    add script to install bookinfo

---
+++ istio/01-bookinfo/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
