12183854dbb8e6b72a692dbafe683e10a772b753
Steven Black <genseb7@gmail.com>
2021-03-11 11:18:32 -0500

    run script wip

--- istio/run.sh
+++ istio/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 
 
+function generate_traffic() {
+export ingress_ip=$(kubectl get svc istio-ingressgateway -n istio-system \
+  -o jsonpath='{.status.loadBalancer.ingress[0].ip}'; echo)
+echo "sending 500 requests to http://$ingress_ip/productpage"
+for i in {1..500}; do curl -s -o /dev/null http://$ingress_ip/productpage; done
+echo 'done!'
+}
+
+
 echo 'instal istioctl cli tool?'; read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 curl -sL https://istio.io/downloadIstioctl | ISTIO_VERSION=1.9.1 sh -
@@ -41,37 +50,49 @@ kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
 fi
 
-# echo 'ready to insall addons?'; read answer
+echo 'ready to install addons?'; read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 kubectl apply -f samples/addons/grafana.yaml
 kubectl apply -f samples/addons/jaeger.yaml
 kubectl apply -f samples/addons/kiali.yaml
 kubectl apply -f samples/addons/prometheus.yaml
+kubectl apply -f samples/addons/extras/zipkin.yaml
 fi
 
 
-# kubectl rollout status deployment/kiali -n istio-system
-# istioctl dashboard kiali &
-# echo 'ready to create traffic?'
-# read
-# export ingress_ip=$(kubectl get svc istio-ingressgateway -n istio-system \
-#   -o jsonpath='{.status.loadBalancer.ingress[0].ip}'; echo)
-# echo "sending 500 requests to http://$ingress_ip/productpage"
-# for i in {1..500}; do curl -s -o /dev/null http://$ingress_ip/productpage; done
-# echo 'done!'
+echo 'ready to run addons?'; read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+kubectl rollout status deployment/kiali -n istio-system
+istioctl dashboard kiali &
 
-# echo 'grafana?'
-# read
-# kubectl rollout status deployment/grafana -n istio-system
-# istioctl dashboard grafana &
+generate_traffic
 
-# echo 'jaeger?'
-# read
-# kubectl rollout status deployment/jaeger -n istio-system
-# istioctl dashboard jaeger &
+echo 'grafana?'
+kubectl rollout status deployment/grafana -n istio-system
+istioctl dashboard grafana &
+
+echo 'jaeger?'
+kubectl rollout status deployment/jaeger -n istio-system
+istioctl dashboard jaeger &
 
 
-# echo 'zipkin?'
-# read
-# kubectl rollout status deployment/zipkin -n istio-system
-# istioctl dashboard zipkin &
+echo 'zipkin?'
+kubectl rollout status deployment/zipkin -n istio-system
+istioctl dashboard zipkin &
+fi
+
+
+echo 'ready to run request routing demo?'; read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+echo 'check kiali at http://localhost:20001/kiali/console/graph/namespaces/'
+generate_traffic
+read
+kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
+kubectl apply -f samples/bookinfo/networking/virtual-service-all-v1.yaml
+echo "check kiali again!"
+generate_traffic
+read
+kubectl apply -f samples/bookinfo/networking/virtual-service-reviews-test-v2.yaml
+generate_traffic
+echo "now log into $ingress_ip/productpage as jason"
+fi
